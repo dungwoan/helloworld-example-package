d00c0b057ecd5f3845e2f8116a6fc5b489c865f9
Huynh Van Tri Dung <47659758+dungwoan@users.noreply.github.com>
2025-06-03 18:05:41 +0700

    Create publisher.php

---
+++ config/publisher.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'publishes' => [
+        'helloworld-assets' => [
+            __DIR__ . '/../../public' => public_path('vendor/helloworld'),
+        ],
+        'helloworld-config' => [
+            __DIR__ . '/../../config' => base_path('packages/Example/HelloWorld/config'),
+        ],
+    ],
+]; 
