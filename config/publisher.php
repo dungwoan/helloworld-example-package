79743b04de4dc5dde4a35faa2877855d0e1ad327
Huynh Van Tri Dung <47659758+dungwoan@users.noreply.github.com>
2025-06-04 17:49:29 +0700

    Update publisher.php

--- config/publisher.php
+++ config/publisher.php
@@ -3,10 +3,10 @@
 return [
     'publishes' => [
         'helloworld-assets' => [
-            __DIR__ . '/../../public' => public_path('vendor/helloworld'),
+            __DIR__ . '/../public' => public_path('vendor/helloworld'),
         ],
         'helloworld-config' => [
-            __DIR__ . '/../../config' => base_path('packages/Example/HelloWorld/config'),
+            __DIR__ . '/../config' => base_path('packages/Example/HelloWorld/config'),
         ],
     ],
 ]; 
